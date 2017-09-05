9f70418488e593e42953665ca3a8beeed35f088c
Joona <joonatoona@digitalfishfun.com>
2017-09-05 17:14:38 +0300

    add copyright notification to all source files

--- src/main/java/com/digitalfishfun/modularmechanisms/proxy/CommonProxy.java
+++ src/main/java/com/digitalfishfun/modularmechanisms/proxy/CommonProxy.java
@@ -1,3 +1,8 @@
+/*
+* Copyright (C) 2017 Joona <joonatoona@digitalfishfun.com>
+* SPDX-License-Identifier: GPL-3.0+
+*/
+
 package com.digitalfishfun.modularmechanisms.proxy;
 
 import net.minecraft.item.Item;
