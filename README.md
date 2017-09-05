c9e246d0ffb59b9b5f29a97d282997c0244575e8
Joona <joonatoona@digitalfishfun.com>
2017-09-05 10:56:28 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Modular Machines
+
+---
+
+Someone needs to come up with a more original name than that.
---
+++ src/main/java/com/digitalfishfun/modularmachines/ModularMachines.java
@@ -0,0 +1,17 @@
+package com.digitalfishfun.modularmachines;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = ModularMachines.MODID, version = ModularMachines.VERSION)
+public class ModularMachines
+{
+    public static final String MODID = "modularmachines";
+    public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+    }
+}
