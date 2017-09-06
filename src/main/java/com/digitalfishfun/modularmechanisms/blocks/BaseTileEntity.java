edd65ecc0e7b4f6b6fc5eb8b737bd9e11c0c1dd5
Joona <joonatoona@digitalfishfun.com>
2017-09-06 11:14:38 +0300

    add copyright header

--- src/main/java/com/digitalfishfun/modularmechanisms/blocks/BaseTileEntity.java
+++ src/main/java/com/digitalfishfun/modularmechanisms/blocks/BaseTileEntity.java
@@ -1,3 +1,8 @@
+/*
+* Copyright (C) 2017 Joona <joonatoona@digitalfishfun.com>
+* SPDX-License-Identifier: GPL-3.0+
+*/
+
 package com.digitalfishfun.modularmechanisms.blocks;
 
 import net.minecraft.block.material.Material;
