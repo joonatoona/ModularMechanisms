78f9a3864bdbc995a44b94e470dfa67321463e04
Joona <joonatoona@digitalfishfun.com>
2017-09-08 15:37:21 +0300

    apply models to press multiblock Note: Rotation will need to be figured out at some point

--- src/main/java/com/digitalfishfun/modularmechanisms/blocks/multiblock/BlockMultiblockBase.java
+++ src/main/java/com/digitalfishfun/modularmechanisms/blocks/multiblock/BlockMultiblockBase.java
@@ -7,8 +7,10 @@ package com.digitalfishfun.modularmechanisms.blocks.multiblock;
 
 import com.digitalfishfun.modularmechanisms.blocks.BaseTileEntity;
 import com.digitalfishfun.modularmechanisms.utils.MMLogger;
+import net.minecraft.block.BlockHorizontal;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyDirection;
 import net.minecraft.block.properties.PropertyInteger;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
